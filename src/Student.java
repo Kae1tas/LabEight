ec546c1ba339745811ac6820f5d1fdb77251f349
Mihail <79074705+Kae1tas@users.noreply.github.com>
2021-11-18 17:10:07 +0900

    1

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,8 @@
 public class Student extends People{
-    public Student(String name, String lastname, int age) {
+    Student(String name, String lastname, int age) {
         super(name, lastname, age);
     }
-    public static void learn(){
+    public static void learn() {
         System.out.println("learn");
     }
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,8 @@
-public class Teacher extends People{
-    public Teacher(String name, String lastname, int age){
+public class Teacher extends People {
+    Teacher(String name, String lastname, int age) {
         super(name, lastname, age);
     }
-    public static void teach(){
+    public static void teach() {
         System.out.println("teach");
     }
 }
